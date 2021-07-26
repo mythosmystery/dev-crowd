11e18a6a51578da91611262c0a7d7ed4d017163f
mythosmystery <axiysfire@gmail.com>
2021-07-26 12:28:32 -0500

    updated mutations and register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,78 +5,74 @@ import Auth from '../utils/auth';
 
 import { Button, Form } from 'react-bootstrap';
 
-
 function Register(props) {
-    const [formState, setFormState] = useState({ email: '', username: '', password: '' });
-    const [createUser] = useMutation(CREATE_USER);
+   const [formState, setFormState] = useState({ email: '', username: '', password: '' });
+   const [createUser] = useMutation(CREATE_USER);
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        const mutationResponse = await createUser({
-            variables: {
-                firstname: formState.formFname,
-                lastname: formState.formLname,
-                username: formState.formUsername,
-                email: formState.formBasicEmail,
-                password: formState.formBasicPassword,
-            },
-        });
-        const token = mutationResponse.date.createUser.token;
-        Auth.login(token);
-    };
+   const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      const mutationResponse = await createUser({
+         variables: {
+            name: formState.formFname,
+            username: formState.formUsername,
+            email: formState.formBasicEmail,
+            password: formState.formBasicPassword,
+         },
+      });
+      const token = mutationResponse.date.createUser.token;
+      Auth.login(token);
+   };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value
-        });
-    };
+   const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormState({
+         ...formState,
+         [name]: value,
+      });
+   };
 
-    return (
-        <div>
-            <h1>Create Account</h1>
-            <p>Please enter your information to create an account.</p>
+   return (
+      <div>
+         <h1>Create Account</h1>
+         <p>Please enter your information to create an account.</p>
 
-            <Form onSubmit={handleFormSubmit}>
-                <Form.Group className="mb-3" controlId="formFname">
-                    <Form.Label>Full Name </Form.Label>
-                    <Form.Control required type="text" placeholder="Enter your name" />
-                </Form.Group>
-                
-                {/* <Form.Group className="mb-3" controlId="formLname">
+         <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="formFname">
+               <Form.Label>Full Name </Form.Label>
+               <Form.Control required type="text" placeholder="Enter your name" />
+            </Form.Group>
+
+            {/* <Form.Group className="mb-3" controlId="formLname">
                     <Form.Label>Last Name </Form.Label>
                     <Form.Control required type="text" placeholder="Enter last name" />
-                </Form.Group> */}                
+                </Form.Group> */}
 
-                <Form.Group className="mb-3" controlId="formUsername">
-                    <Form.Label>Username </Form.Label>
-                    <Form.Control required type="text" placeholder="Enter username" onChange={handleChange} />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formUsername">
+               <Form.Label>Username </Form.Label>
+               <Form.Control required type="text" placeholder="Enter username" onChange={handleChange} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address </Form.Label>
-                    <Form.Control required type="email" placeholder="Enter email" onChange={handleChange} />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+               <Form.Label>Email address </Form.Label>
+               <Form.Control required type="email" placeholder="Enter email" onChange={handleChange} />
+               <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password </Form.Label>
-                    <Form.Control required type="password" placeholder="Password" onChange={handleChange} />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+               <Form.Label>Password </Form.Label>
+               <Form.Control required type="password" placeholder="Password" onChange={handleChange} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check required type="checkbox" label="I agree to terms & service" />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+               <Form.Check required type="checkbox" label="I agree to terms & service" />
+            </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
-    );
+            <Button variant="primary" type="submit">
+               Submit
+            </Button>
+         </Form>
+      </div>
+   );
 }
 
 export default Register;

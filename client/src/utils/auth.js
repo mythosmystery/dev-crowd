67d3dfcfcc6e3adacd2e93ef1d66f9964681dd36
Asha Kumar <ashakumar90@gmail.com>
2021-07-27 13:40:50 -0400

    fixed the create profile path

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,7 +35,7 @@ class AuthService {
    login(idToken) {
       // Saves user token to localStorage
       localStorage.setItem('id_token', idToken);
-      window.location.assign('/users/:userId');
+      window.location.assign('/users/:id');
    }
 
    logout() {

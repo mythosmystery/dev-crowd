bd2d6795c4bd89cbd945060a63cf4dc17b29ec4f
mythosmystery <axiysfire@gmail.com>
2021-11-12 02:19:52 -0600

    fixed comment bug

--- client/src/components/MakeComment.js
+++ client/src/components/MakeComment.js
@@ -8,24 +8,24 @@ function MakeComment({ refetch, postId }) {
    const [formState, setFormState] = useState({ content: '', postId });
    const [addComment, { error }] = useMutation(ADD_COMMENT);
 
-   const onSubmit = async (event) => {
+   const onSubmit = async event => {
       event.preventDefault();
       console.log(formState);
       try {
          const { data } = await addComment({ variables: { ...formState } });
          if (data.addComment) {
             refetch();
-            setFormState({ content: '' });
+            setFormState({ content: '', postId });
          }
       } catch (err) {
          console.error(err);
       }
    };
-   const onChange = (event) => {
+   const onChange = event => {
       const { name, value } = event.target;
       setFormState({
          ...formState,
-         [name]: value,
+         [name]: value
       });
    };
 
@@ -35,13 +35,18 @@ function MakeComment({ refetch, postId }) {
             <Form onSubmit={onSubmit}>
                <Form.Group>
                   <Form.Control
-                     type="content"
-                     placeholder={Auth.loggedIn() ? "Add a comment..." : 'Please log in to comment'}
-                     name="content"
+                     type='content'
+                     placeholder={Auth.loggedIn() ? 'Add a comment...' : 'Please log in to comment'}
+                     name='content'
                      value={formState.content}
                      onChange={onChange}
                   />
-                  <Button type="submit" color="dark blue" disabled={!formState.content || !Auth.loggedIn()} className="my-2">
+                  <Button
+                     type='submit'
+                     color='dark blue'
+                     disabled={!formState.content || !Auth.loggedIn()}
+                     className='my-2'
+                  >
                      Comment!
                   </Button>
                </Form.Group>
